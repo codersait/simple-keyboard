6f294822ca68d13695a30a8030e82890684deb86
codersait <codersait@gmail.com>
2020-08-26 13:59:39 +0300

    initial commit

---
+++ precache-manifest.775030bf43f290dd7a9b2665d478160c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec45a4d617030b1dde59b34db046972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c6460b1cbb849c6e3e",
+    "url": "/static/js/2.c508b179.chunk.js"
+  },
+  {
+    "revision": "1fe25bdf3f82bd3b47e2",
+    "url": "/static/js/main.8db6fb66.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
